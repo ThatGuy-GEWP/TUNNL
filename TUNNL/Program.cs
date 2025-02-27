6b6c00fa1531060ff122a2985556cbdd4ecc3c0b
timothy anderson <timmy.anderson.aol.com@gmail.com>
2025-02-27 09:19:42 -0500

    Add project files.

---
+++ TUNNL/Program.cs
@@ -0,0 +1,36 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using SFML_GE.System;
+
+namespace TUNNL
+{
+    public class Program
+    {
+        public static RenderWindow App { get; private set; } = new RenderWindow(
+            new VideoMode(1280, 720),
+            "SFML-GE Template",
+            Styles.Close | Styles.Titlebar
+        );
+
+        static void Main(string[] args)
+        {
+            bool appOpen = true;
+
+            App.Closed += (a, args) => { App.Close(); appOpen = false; };
+            App.SetFramerateLimit(144);
+
+            Project mainProject = new Project("Res", App);
+            Scene scene = mainProject.CreateSceneAndLoad("DefaultScene");
+
+            mainProject.Start();
+            while (appOpen)
+            {
+                mainProject.Update();
+
+                App.Clear();
+                mainProject.Render(App);
+                App.Display();
+            }
+        }
+    }
+}
